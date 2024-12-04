21f1a0a208805d71c2762ee7ae39404d2dd87c58
mariiaz09 <142826683+mariiaz09@users.noreply.github.com>
2024-12-04 07:16:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,6 @@ from google.cloud import datastore
 import google.oauth2.id_token
 
 firebase_request_adapter = requests.Request()
-datastore_client = datastore.Client()
-app = Flask(__name__)
-
-
-
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key("visit"))
-    entity.update({"timestamp": dt})
-    datastore_client.put(entity)
-
-def fetch_times(limit):
-    query = datastore_client.query(kind="visit")
-    query.order = ["-timestamp"]
-    times = query.fetch(limit=limit)
-    return times
-
 @app.route("/")
 def root():
     # Verify Firebase auth.
@@ -32,15 +16,22 @@ def root():
 
     if id_token:
         try:
-            # Verify the token against the Firebase Auth API.
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
             claims = google.oauth2.id_token.verify_firebase_token(
                 id_token, firebase_request_adapter
             )
         except ValueError as exc:
-            # This will be raised if the token is expired or any other verification fails.
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
             error_message = str(exc)
 
-        # Record and fetch the recent times a logged-in user has accessed the site.
+        # Record and fetch the recent times a logged-in user has accessed
+        # the site. This is currently shared amongst all users, but will be
+        # individualized in a following step.
         store_time(datetime.datetime.now(tz=datetime.timezone.utc))
         times = fetch_times(10)
 
@@ -48,12 +39,3 @@ def root():
         "index.html", user_data=claims, error_message=error_message, times=times
     )
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
